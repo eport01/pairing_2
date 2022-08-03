ffe81904cbbec5c3cb05f311c847451b0a5c5b3d
Emily Port <emilylport@gmail.com>
2022-08-03 15:18:41 -0600

    Add if statement

--- friends.rb
+++ friends.rb
@@ -1,6 +1,14 @@
-best_friend = "Frank"
+print "What is your name? "
+
+best_friend = gets.chomp
+
+
+
 if best_friend == "Frank"
   puts "You're my best friend!"
+elsif best_friend == "Woody"
+  puts "You're my second best friend!"
 else
   puts "Do I know you?"
+
 end
